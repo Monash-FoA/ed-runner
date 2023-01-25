9a2237c4e174b49a66d8e6d5404a35acccab916d
glipR <37640160+glipR@users.noreply.github.com>
2023-01-25 18:46:30 +1100

    Create everything.

---
+++ test_actual/test_basics.py
@@ -0,0 +1,38 @@
+import unittest
+from ed_utils.decorators import *
+
+class TestThings(unittest.TestCase):
+
+    @number("1.1")
+    @advanced()
+    def test_basic(self):
+        print("HI")
+        self.assertEqual(True, False)
+
+    @number("1.2")
+    @weight(3)
+    def test_basic_2(self):
+        print("HI AGAIN")
+        self.assertEqual(True, True)
+
+    @number("1.3")
+    @advanced()
+    @hide_errors("You can't see me")
+    def test_basic_3(self):
+        print("HI ALWAYS\n")
+        self.assertEqual(True, False)
+
+    @number("2.1")
+    @visibility(visibility.VISIBILITY_HIDDEN)
+    def test_more(self):
+        self.assertEqual(True, False)
+
+    @number("2.2")
+    @visibility(visibility.VISIBILITY_PRIVATE)
+    def test_more_2(self):
+        self.assertEqual(True, False)
+
+    @number("2.3")
+    @visibility(visibility.VISIBILITY_SHOW)
+    def test_more_3(self):
+        self.assertEqual(True, True)
