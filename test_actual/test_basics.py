8181b1f0bd5684a6c8b1f0ee367adef09b6a00d0
glipR <37640160+glipR@users.noreply.github.com>
2023-02-05 10:17:26 +1100

    Add timeout.

--- test_actual/test_basics.py
+++ test_actual/test_basics.py
@@ -1,21 +1,25 @@
 import unittest
 from ed_utils.decorators import *
+from ed_utils.timeout import timeout
 
 class TestThings(unittest.TestCase):
 
     @number("1.1")
+    @timeout()
     @advanced()
     def test_basic(self):
         print("HI")
         self.assertEqual(True, False)
 
     @number("1.2")
+    @timeout()
     @weight(3)
     def test_basic_2(self):
         print("HI AGAIN")
         self.assertEqual(True, True)
 
     @number("1.3")
+    @timeout()
     @advanced()
     @hide_errors("You can't see me")
     def test_basic_3(self):
@@ -23,16 +27,19 @@ class TestThings(unittest.TestCase):
         self.assertEqual(True, False)
 
     @number("2.1")
+    @timeout()
     @visibility(visibility.VISIBILITY_HIDDEN)
     def test_more(self):
         self.assertEqual(True, False)
 
     @number("2.2")
+    @timeout()
     @visibility(visibility.VISIBILITY_PRIVATE)
     def test_more_2(self):
         self.assertEqual(True, False)
 
     @number("2.3")
+    @timeout()
     @visibility(visibility.VISIBILITY_SHOW)
     def test_more_3(self):
         self.assertEqual(True, True)
